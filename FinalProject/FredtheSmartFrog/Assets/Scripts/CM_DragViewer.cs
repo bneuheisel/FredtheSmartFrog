747b24750bf548e67596533fa5c2f79eab7a02c4
Andrew Lam <andrewlam468@gmail.com>
2019-11-20 22:26:22 -0700

    NavMesh and camera update
    Lowered some doors and added invisible cubes to manipulate the navmesh. These changes are meant to allow smooth passes through certain doors. CM_DragViewer script modified to allow zooming in or out and with lowered sensitivity.

--- FinalProject/FredtheSmartFrog/Assets/Scripts/CM_DragViewer.cs
+++ FinalProject/FredtheSmartFrog/Assets/Scripts/CM_DragViewer.cs
@@ -17,7 +17,8 @@ public class CM_DragViewer : MonoBehaviour
     void Update()
     {
         CinemachineTransposer ct = _vcam.GetCinemachineComponent<CinemachineTransposer>();
-        ct.m_FollowOffset = Quaternion.AngleAxis(_touchfield.TouchDist.x, Vector3.up) * ct.m_FollowOffset;
-        ct.m_FollowOffset = Quaternion.AngleAxis(_touchfield.TouchDist.y, Vector3.right) * ct.m_FollowOffset;
+        ct.m_FollowOffset = Quaternion.AngleAxis(_touchfield.TouchDist.x * 0.25f, Vector3.up) * ct.m_FollowOffset;
+        //ct.m_FollowOffset = Quaternion.AngleAxis(_touchfield.TouchDist.y, Vector3.right) * ct.m_FollowOffset;
+        ct.m_FollowOffset += Vector3.up * _touchfield.TouchDist.y * 0.25f;
     }
 }
